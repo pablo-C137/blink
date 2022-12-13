5dc5c5f0ded34542667a83945844f684b2333387
maina_kennedy <98929270+pablo-C137@users.noreply.github.com>
2022-12-13 11:25:40 +0300

    change_done

--- blink.ino
+++ blink.ino
@@ -15,7 +15,7 @@ void setup() {
   pinMode(led, OUTPUT);     
 }
 
-// the loop routine runs over and over again forever:
+// the loop routine rus over and over again forever:
 void loop() {
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);               // wait for a second
